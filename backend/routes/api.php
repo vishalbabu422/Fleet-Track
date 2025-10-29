99130856a3bc0c91f391c6bb7cbc824956428d4c
Vishal Babu <vishalbabu422@gmail.com>
2025-10-29 22:39:00 +0530

    'orders'

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrdersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum','role:admin,dispatcher'])->group(function(){
+    Route::get('/orders',[OrdersController::class,'index']);
+});
